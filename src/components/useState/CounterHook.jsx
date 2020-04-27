78eb60ab1d04320d00aad81870ecb2c8265e970b
cidermitaina <cidermitaina@gmail.com>
2020-04-27 14:56:58 +0900

    update

--- src/components/useState/CounterHook.jsx
+++ src/components/useState/CounterHook.jsx
@@ -11,16 +11,10 @@ export const CounterHook = () => {
     setCount((prevCount) => prevCount + 1);
   };
 
-  const incrementCountTen = () => {
-    for (let i = 0; i < 10; i++) {
-      setCount((prevCount) => prevCount + 1); //更新前の値に基づいて計算される場合はprevious valueを使う
-    }
-  };
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={incrementCount}>カウント + 1</button>
-      <button onClick={incrementCountTen}>カウント + 10</button>
     </div>
   );
 };
